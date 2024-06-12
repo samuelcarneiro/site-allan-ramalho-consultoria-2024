95a414a8c6901515170d5725807a31df6605398b
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-06-12 14:15:43 -0300

    Adição de 'função' para atualizar o ano do footer automaticamente.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,16 @@ function menuShow() {
         document.querySelector('.icon-menu').src="./assets/img/icon-menu-close.png";
     }
 }
+
+/* Mostrar ano atual no footer */
+const ano = document.getElementById("mostrarAnoAtual");
+const anoAtual = new Date();
+ano.innerHTML = anoAtual.getFullYear();
+
+var root = document.documentElement;
+root.className += ' js';
+
+function boxTop(idBox) {
+	var boxOffset = $(idBox).offset().top;
+	return boxOffset;
+}
